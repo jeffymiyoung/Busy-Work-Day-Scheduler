346de907a67568716448c95f859b1250d4e9f5b2
Jeffrey Young <jeffreymiyoung@aol.com>
2022-04-10 22:36:23 -0400

    fixed getItem issue ~ misspelling

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,19 +48,17 @@ $(document).ready(function () {
     })
 
     // get localStorage data for each hour specification
-    $("#hour8 .desciption").text(function () {
-        JSON.parse(localStorage.getItem("hour8").val());
-    });
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    $("#hour9 .desciption").val(localStorage.getItem("hour9"));
-    $("#hour10 .desciption").text(localStorage.getItem("hour10"));
-    $("#hour11 .desciption").text(localStorage.getItem("hour11"));
-    $("#hour12 .desciption").val(localStorage.getItem("hour12"));
-    $("#hour13 .desciption").val(localStorage.getItem("hour13"));
-    $("#hour14 .desciption").val(localStorage.getItem("hour14"));
-    $("#hour15 .desciption").val(localStorage.getItem("hour15"));
-    $("#hour16 .desciption").val(localStorage.getItem("hour16"));
-    $("#hour17 .desciption").val(localStorage.getItem("hour17"));
 
     timeKeeper();
 });
